764a6449c33d43e3757819ec6b50561c25bb77e5
seongwook8 <seongwook.choi@gatech.edu>
2021-12-04 18:29:25 -0500

    final_version of extract_data

--- extract_data.py
+++ extract_data.py
@@ -4,66 +4,144 @@ import pandas as pd
 import random
 import string
 from spotipy.oauth2 import SpotifyClientCredentials
+import numpy as np
+from difflib import SequenceMatcher
+import time
+import gc
+
+
 
 # please make sure your spotify api is activated before running
+# accesses the billboard api to obatin all the weekly charts for a specific year.
+# using those charts audio features have been extracted from the spotify api
+# and saved as .csv files.
 
-SPOTIPY_CLIENT_ID="a73fd176465040b4815ae85f848cd2c9"
-SPOTIPY_CLIENT_SECRET="f93b8a63b9704dbf883fe5072ef64c7d"
-sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET))
-#auth_manager = SpotifyClientCredentials()
-#sp = spotipy.Spotify(auth_manager=auth_manager)
+def getWeekly(chart, sp):
 
-start = 2009
-end = 2020
-
-for i in range(start, end + 1):
-
-    year = str(i)
-
-# create chart for the given year
-    chart = billboard.ChartData('hot-100', date = year+'-12-31')
-
-# create a separate chart with just song title and song artist
-    songs = [[song.title, song.artist] for song in chart]
-
-    output = []
+    year = chart.date[:4]
+    data = []
+    index = 0
     hashmap = {}
+    in_top_100 = []
 
-# obtain song attributes from the spotify api and append the results into output
-    stop_words=["featuring", "x", "With", "&", "with", "of", "the", "or", "patrick", "(featuring", "t-pain)", "bow", "wow", "duet", "stump", "Featuring", "And", "Introducing"]
+    while chart.previousDate:
+        print(chart.date)
+
+        # For creating entire .csv file
+        songs = []
+
+        # processing to match the song name from billboards with spotify.
+        for song in chart:
+            arr = []
+            arr.append(song.title)
+            arr.append(song.artist)
+
+            title = song.title.split(" ")
+            artist = song.artist.split(" ")
+
+            if len(title) > 2:
+                title = title[:2]
+                
+            title = ' '.join(title)
+
+            if len(artist) > 2:
+                artist = artist[:2]
+            artist = ' '.join(artist)
+            artist = artist.replace(' Featuring', '').replace(' X', '').replace(' x', '').replace(' &', '')
+            
+            arr.append(title)
+            arr.append(artist)
+            arr.append(song.weeks)
+
+            songs.append(arr)
+
+            del arr
+            del title
+            del artist
+
+
+        output = []
+
+        # extract data from spotify
+        for song in songs:
+            while True:
+                try:
+                    results = sp.search(q = ' '.join([song[3], song[2]]), type = 'track')
+                except:
+                    time.sleep(30)
+                    continue
+                break
+
+            try:
+                name = results['tracks']['items'][0]['name']
+                artists = [artist['name'] for artist in results['tracks']['items'][0]['artists']]
+                ratio1 = SequenceMatcher(None, name, song[0]).real_quick_ratio()
+                ratio2 = SequenceMatcher(None, artists[0], song[1]).real_quick_ratio()
+                id = results['tracks']['items'][0]['id']
+                features = list(sp.audio_features(id)[0].values())
+                output.append([(ratio1 + ratio2)/2, song[0], name, song[1], artists, id, *features[0:11], *features[16:], song[4]])
+                if name not in hashmap:
+                    hashmap[name] = 1
+                    in_top_100.append([(ratio1 + ratio2)/2, song[0], name, song[1], artists, id, *features[0:11], *features[16:], song[4]])
+                del name
+                del artists
+                del ratio1
+                del ratio2
+                del id
+                del features
+            except:
+                output.append([0, song[0], '', song[1], ''])
+            
+            del results
+            del song
+
+
+        # save the data as .csv
+        df = pd.DataFrame(output)
+        df.columns = ['match','billboard_track', 'spotify_track', 'billboard_artist','spotify_artist', 'spotify_id', 'danceability', 'energy', 'key', 'loudness', 'mode', 'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo', 'duration_ms', 'time_signature', 'weeks']
+        df.to_csv('weeks/' + chart.date + '.csv')
+        previous_date = chart.previousDate
+        del output
+        del df
+        del chart
+        chart = billboard.ChartData('hot-100', previous_date)
+
+        # if all the weeks have been accessed, save a compiled data into a year
+        # and return to main method.
+
+        if chart.date[:4] != year:
+            print(year)
+            df = pd.DataFrame(in_top_100)
+            df.columns = ['match','billboard_track', 'spotify_track', 'billboard_artist','spotify_artist', 'spotify_id', 'danceability', 'energy', 'key', 'loudness', 'mode', 'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo', 'duration_ms', 'time_signature', 'weeks']
+            df.to_csv('years/' + year + '.csv')
+            year = chart.date[:4]
+            hashmap = {}
+            in_top_100 = []
+            del df
+            del in_top_100
+            del hashmap
+
+            return chart.date
+        
+        del songs
+        gc.collect()
+
+
+# main method where you can specify the start and end year to extract data
+def main():
+    auth_manager = SpotifyClientCredentials()
+    sp = spotipy.Spotify(auth_manager=auth_manager)
+
+    start = 2021
+    end = 2010
+    chart = billboard.ChartData('hot-100', f"{start}-12-31")
+
+    while start > end:
+        date = getWeekly(chart, sp)
+        chart = billboard.ChartData('hot-100', date)
+        start = int(date[:4])
+
     
-    for song in songs:
-        artist=song[1]
-        artist=[s.lower() for s in artist.split() if s.lower() not in stop_words]
-        artist=(' '.join(artist))
-        #print(artist)
-        results = sp.search(q = 'track:' + song[0] + ' ' + 'artist:' + artist, type = 'track')
-        try:
-            name = results['tracks']['items'][0]['name']
-            hashmap[name] = 1
-            artists = [artist['name'] for artist in results['tracks']['items'][0]['artists']]
-            id = results['tracks']['items'][0]['id']
-            features = list(sp.audio_features(id)[0].values())
-            output.append([name, artists, id] + features[0:11] + features[16:])
-        except:
-            output.append([song[0], song[1]])
 
-# randomly choose 100 songs from spotify within the given year and append the results into output
-    while len(output) < 200:
-        alph = random.choice(string.ascii_letters)
-        results = sp.search(q = 'year:' + year + ' ' + 'track:' + alph, limit = 50, type = 'track')
-        tracks = results['tracks']['items']
-        rand_num = random.randint(0, len(tracks)-1)
-        name = tracks[rand_num]['name']
-        if name not in hashmap:
-            hashmap[name] = 1
-            artists = [artist['name'] for artist in tracks[rand_num]['artists']]
-            id = tracks[rand_num]['id']
-            features = list(sp.audio_features(id)[0].values())
-            output.append([name, artists, id] + features[0:11] + features[16:])
-
-# save the output into csv file
-    df = pd.DataFrame(output)
-    df.columns = ['track', 'artist', 'spotify_id', 'danceability', 'energy', 'key', 'loudness', 'mode', 'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo', 'duration_ms', 'time_signature']
-
-    df.to_csv('clean_data_2007_2020/'+year+'.csv')
+if __name__ == "__main__":
+    main()
